56b1a472432cc37318f19a3f4f67ef0d81280122
Nuno Gonçalves <nuno.goncalves@oracle.com>
2022-03-18 16:26:14 +0100

    additional developments

--- variables.tf
+++ variables.tf
@@ -7,9 +7,41 @@ variable "region" {}
 variable "fingerprint" {}
 variable "private_key_path" {}
 variable "user_ocid" {}
-variable "availability_domain_name" {}
 
 variable "release" {
   description = "Reference Architecture Release (OCI Architecture Center)"
   default     = "1.0"
+}
+
+variable "project_description" {
+  default = "My Vision AI Project"
+}
+
+variable "project_display_name" {
+  default = "my-project"
+}
+
+variable "model_description" {
+  default = "My Vision AI Model"
+}
+
+variable "model_display_name" {
+  default = "my-model"
+}
+
+variable "model_model_type" {
+  default = "OBJECT_DETECTION"
+}
+
+variable "model_training_dataset_dataset_type" {
+  default = "OBJECT_STORAGE"
+}
+
+variable "model_training_dataset_bucket" {
+}
+
+variable "model_training_dataset_namespace" {
+}
+
+variable "model_training_dataset_object" {
 }
